97ec60630c5eebbdc7712592d4eaf0b896949312
Jade <littleyu.hit@gmail.com>
2015-01-10 17:48:51 +0800

    change

--- static/src/js/aphrodite/image.js
+++ static/src/js/aphrodite/image.js
@@ -1,3 +1,8 @@
 $(function(){
-    avalon.ajaxupload.upload('/apis/aphrodite/image.json', $('input[name="image"]'));
+    $('input[name="upload"]').click(function(){
+        avalon.ajaxupload.upload(
+            '/apis/aphrodite/image.json', 
+            $('input[name="image"]')
+        );
+    });
 })
